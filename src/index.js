2cc145f5e0caa69ce0a0d3e4bb53dbc1b9357fee
amy <amy@lfuturedev.com>
2019-05-27 17:25:02 +0630

    Structing Data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+require('./db/mongoose');
+const User =require('./models/user');
+
+const app = express();
 const port = process.env.port || 3000
 
+app.use(express.json());
+ 
 app.post('/users', (req, res) => {
-    res.send('testing');
+
+    
+    const user = new User(req.body);
+
+    user.save().then((result)=>{
+        res.send(user);
+    }).catch((e)=>{
+        res.status(400);
+        res.send(e);
+    });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
