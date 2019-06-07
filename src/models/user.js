48a33457ed1b5283cfdc29544041603d948b5f52
Amy <ampyaephyonaing@gmail.com>
2019-06-07 07:20:36 +0630

    User Login, Hash Password, Add Middleware

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-
-const User =mongoose.model('User',{
+const bcrypt = require('bcryptjs');
+const userSchema = new mongoose.Schema({
     name:{
         type:String,
         required:true,
@@ -19,6 +19,7 @@ const User =mongoose.model('User',{
     },
     email:{
         type:String,
+        unique:true,
         required:true,
         trim:true,
         validate(value){
@@ -26,7 +27,7 @@ const User =mongoose.model('User',{
                     throw new Error('Email is invalid');
             }
         }
-    },
+    }, 
     age:{
         type:Number,
         required:true,
@@ -37,5 +38,35 @@ const User =mongoose.model('User',{
         }
     }
 });
+
+userSchema.statics.findByCredentials = async (email, password) =>{
+    const user = await User.findOne({email})
+    
+    if(!user){
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch){
+        throw new Error('Unable to login')
+    }
+
+    return user;
+}
+
+//Hash plain text password before saving
+userSchema.pre('save', async function(next){
+    const user = this
+   if(user.isModified('password')){
+       user.password = await bcrypt.hash(user.password,8);
+   }
+
+
+    next()
+});
+
+
+const User =mongoose.model('User',userSchema);
 
 module.exports = User;
