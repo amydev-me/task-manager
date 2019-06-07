e04517bc941d37837c89d3f7f788b605cc427b48
Amy <ampyaephyonaing@gmail.com>
2019-06-08 00:32:26 +0630

    Add middleware & Get Profile as an Authorize User

---
+++ src/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const auth = async (req, res, next) =>{
+    try {
+        const token = req.header('Authorization').replace("Bearer ",'')
+        
+        const decode = jwt.verify(token, 'thisismynewcourse')
+
+        const user = await User.findOne({_id:decode._id,'tokens.token':token});
+
+        if(!user){
+            throw new Error();
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        // return res.send(error)
+        return res.status(401).send({error:"Please authenticate."})
+    }
+}
+
+module.exports = auth;
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router  = new express.Router();
 const User = require('../models/user');
-
+const auth = require('../middleware/auth');
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
     try{
         await user.save();
-        res.status(201).send(user);
+        await user.generateAuthToken();
+
+        res.status(201).send({user,token});
     }catch(e){
         res.status(500).send(e);
     }
@@ -16,13 +18,17 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res)=>{
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password);
-        res.send(user);
+        const token = await user.generateAuthToken();
+        res.send({user,token});
     } catch (error) {
         res.status(400).send(error);
     }
 })
+router.get('/users/me',auth, async (req, res)=>{
+    res.send(req.user);
+});
 
-router.get('/users',async (req, res)=>{
+router.get('/users',auth,async (req, res)=>{
     try{
         const users = await User.find({});
         res.send(users);
@@ -69,11 +75,11 @@ router.patch('/users/:id',async  (req, res)=>{
         if(!user){
             return res.status(404).send();
         }
-
+ 
         res.send(user);
 
     }catch(e){
-        res.status(500).send(e);
+        res.status(500).send();
     }
 });
 
